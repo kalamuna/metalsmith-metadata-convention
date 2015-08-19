f535c62044420c1c02501649c43d8f54cde67296
Rob Loach <robloach@gmail.com>
2015-08-19 15:42:41 -0400

    Add async support

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v0.0.2: 2015-08-19
+
+- Added async support
+
 ## v0.0.1: 2015-07-06
 
 - Initial release
--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 'use strict'
 
 var path = require('path')
+var async = require('async')
 
 module.exports = function (opts) {
   return function (files, metalsmith, done) {
     var metadata = metalsmith.metadata()
-    for (var file in files) {
+
+    /**
+     * Process a file, saving it as metadata if needed.
+     */
+    function processFile (file, filename, callback) {
       // Check if it matches the convention.
-      if (path.extname(file) === '.metadata') {
-        // Load in the metadata.
-        metadata[path.basename(file, '.metadata')] = files[file]
-        // Remove the file since we've processed it.
-        delete files[file]
+      if (path.extname(filename) === '.metadata') {
+        // Find the name of the metadata.
+        var name = path.basename(filename, '.metadata')
+
+        // Save the meta data.
+        metadata[name] = file
+
+        // Remove the file since we've now processed it.
+        delete files[filename]
       }
+
+      callback()
     }
 
-    done()
+    async.forEachOf(files, processFile, done)
   }
 }
