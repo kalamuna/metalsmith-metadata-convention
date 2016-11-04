ce930ed8be9b431cd1d28ade4d12d7a341dd108d
Rob Loach <robloach@gmail.com>
2016-11-04 13:57:36 -0400

    Add test for subfolders

--- test/index.js
+++ test/index.js
@@ -16,9 +16,11 @@ it('should match metadata from file conventions', function (done) {
       // Ensure the metadata was loaded correctly.
       var metadata = metalsmith.metadata()
       var someMetaData = metadata.someMetaData || {}
+      var othermeta = metadata.othermeta || {}
 
       // Check if the property is correct.
       assert.equal('bar', someMetaData.foo || false)
+      assert.equal('foo', othermeta.kung || false)
 
       // Check whether the files were build just file.
       assertDir('test/fixtures/basic/build', 'test/fixtures/basic/expected')
